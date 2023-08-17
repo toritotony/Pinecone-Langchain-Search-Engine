893e62a097a43fd410470888ca4acda427090ca4
toritotony <wolfeanthony584@gmail.com>
2023-08-17 07:07:10 -0700

    Changes to primary code
    will test functionality in executable python code once jupyter notebook is completely working

--- book-semantic-search.py
+++ book-semantic-search.py
@@ -2,16 +2,40 @@ from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, P
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from tqdm.autonotebook import tqdm
 import pinecone
 import os
 
 loader = PyPDFLoader("../data/field-guide-to-data-science.pdf")
 data = loader.load()
 
-print (f'You have {len(data)} document(s) in your data')
-print (f'There are {len(data[30].page_content)} characters in your document')
-
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
 texts = text_splitter.split_documents(data)
 
-print (f'Now you have {len(texts)} documents')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', '9d837134-08fe-4023-a178-f361ae686679')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'gcp-starter')
+
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+index_name = "wolfe-test"
+
+docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+
+query = "What's the easiest way to integrate?"
+docs = docsearch.similarity_search(query)
+
+llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+chain = load_qa_chain(llm, chain_type="stuff")
+
+chain.run(input_documents=docs, question=query)
+
