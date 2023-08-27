2049cdfd061a8d8b171a403f71f17643790443a5
toritotony <wolfeanthony584@gmail.com>
2023-08-27 09:39:41 -0700

    Now allows links to pdfs and introducing video/other files type features

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from tqdm import tqdm
 import pinecone
+import requests
+import openai
 
 app = Flask(__name__)
 
@@ -39,6 +41,50 @@ def perform_query(index, query):
 
     response = chain.run(input_documents=docs, question=query)
     return response
+
+@app.route('/pdf_search', methods=['POST'])
+def pdf_search():
+    pdf_link = request.form['pdf_link']
+    query = request.form['query']
+
+    if pdf_link and query:
+        pdf_response = requests.get(pdf_link)
+        if pdf_response.status_code == 200:
+            pdf_path = 'downloaded_pdf.pdf'
+            with open(pdf_path, 'wb') as pdf_file:
+                pdf_file.write(pdf_response.content)
+
+            index = index_pdf(pdf_path)
+            results = perform_query(index, query)
+
+            return render_template('search_results.html', query=query, results=results)
+        else:
+            return "Failed to download the PDF from the provided link."
+
+    return "Please provide both a PDF link and a query."
+
+@app.route('/video_transcribe', methods=['POST'])
+def video_transcribe():
+    video_link = request.form['video_link']
+    query = request.form['query']
+
+    if video_link and query:
+        openai.api_key = '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'  # Replace with your actual OpenAI API key
+
+        transcription_result = openai.SpeechApi.create(data={'audio_url': video_link})
+
+        if 'text' in transcription_result:
+            transcribed_text = transcription_result['text']
+
+            # Index the transcribed text and perform queries
+            index = index_pdf(transcribed_text)
+            results = perform_query(index, query)
+
+            return render_template('search_results.html', query=query, results=results)
+        else:
+            return "Failed to transcribe the provided video link."
+
+    return "Please provide both a video link and a query."
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
