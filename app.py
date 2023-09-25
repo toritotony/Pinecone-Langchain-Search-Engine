142320933aa9710a34caff3dccf7fb28c4c6d3f4
toritotony <wolfeanthony584@gmail.com>
2023-09-24 20:02:18 -0700

    Add functions and fix flask video function

--- app.py
+++ app.py
@@ -1,43 +1,88 @@
+# External imports
 import os
+import requests
+import logging
+import tempfile
+import mimetypes
+import docx2txt
+from io import BytesIO
+from PyPDF2 import PdfReader
+from pytube import YouTube
 from flask import Flask, render_template, request
+from flask_limiter import Limiter
+from werkzeug.utils import secure_filename
+
+
+# Internal imports
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-import pinecone
-import requests
-import openai
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
-import tempfile
-import mimetypes
-import docx2txt 
-from io import BytesIO
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from pytube import YouTube
-import speech_recognition as sr
 import whisper
+import pinecone
+import config
 
+# Initialize Flask app
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limits uploads to 16MB
+
+# Configuration and Logging
+logging.basicConfig(level=logging.DEBUG)
+app.logger.info('Info level log')
+app.logger.warning('Warning level log')
+
+UPLOAD_FOLDER = 'C:\Users\wolfe\OneDrive\Desktop\pinecone-test\upload-folder'
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', config.OPENAI_API_KEY)
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', config.PINECONE_API_KEY)
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', config.PINECONE_API_ENV)
+
+if not OPENAI_API_KEY:
+    app.logger.error('OPENAI_API_KEY not set in environment variables!')
+    raise ValueError('Required environment variable not set!')
+
+if not PINECONE_API_KEY:
+    app.logger.error('PINECONE_API_KEY not set in environment variables!')
+    raise ValueError('Required environment variable not set!')
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx'}
+
+# Load external services
+model = whisper.load_model("base")
+
+# Request Limiting Configuration
+def get_remote_address():
+    return request.remote_addr
+
+limiter = Limiter(app, key_func=get_remote_address)  # Adjust based on your needs
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def check_content(content):
+    # Call a content moderation API
+    response = requests.post('https://content-moderation-api.example.com/check', data={'text': content})
+    if response.json().get('safe'):
+        return True
+    else:
+        return False, response.json().get('reason')
+
+def index_plain_text(content):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+    texts = text_splitter.split_documents(content)
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    index_name = "wolfe-test"
+    docsearch = Pinecone.from_texts(texts, embeddings, index_name=index_name)
+    return docsearch
 
 def index_pdf(content):
     if isinstance(content, str):  # Plain text content
-        texts = [content]
-    else:  # Assuming it's a Document instance
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-        texts = text_splitter.split_documents(content)
-
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
-    PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', 'c538f9c7-d734-4326-8e2f-a10d88a5cfd7')
-    PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'gcp-starter')
-
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
-    index_name = "wolfe-test"
-    docsearch = Pinecone.from_texts(texts, embeddings, index_name=index_name)
-
-    return docsearch
+        return index_plain_text(content)
+    else:  # Assuming it's a Document instance, future handling for other document types can go here.
+        return index_plain_text(content)
 
 def extract_text_from_pdf(pdf_path):
     pdf_reader = PdfReader(pdf_path)
@@ -48,16 +93,25 @@ def extract_text_from_pdf(pdf_path):
  
 def perform_query(index, query):
     docs = index.similarity_search(query)
-
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', config.OENAI_API_KEY)
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
     chain = load_qa_chain(llm, chain_type="stuff")
-
     response = chain.run(input_documents=docs, question=query)
     return response
+    
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)  # Make sure filename is secure
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return 'File uploaded successfully'
+    else:
+        return "Invalid file type."
 
 @app.route('/search', methods=['POST'])
 def search():
+    app.logger.debug('Received search request.')
     try:
         pdf = request.files['pdf']
         query = request.form['query']
@@ -75,14 +129,13 @@ def search():
                 index = index_pdf(txt_content)
             else:
                 return "Unsupported file format."
-
             results = perform_query(index, query)
+            app.logger.debug('Search completed successfully.')
             return render_template('search_results.html', query=query, results=results)
-
         return "Please provide both a document and a query."
     except Exception as e:
-        return render_template('error.html',error_message=str(e))
-
+        app.logger.error(f"Error encountered during search: {str(e)}")
+        return render_template('error.html', error_message=str(e))
 
 @app.route('/index_from_link', methods=['POST'])
 def index_from_link():
@@ -91,17 +144,14 @@ def index_from_link():
         query = request.form['query']
 
         if link and query:
-            # Create a temporary file to store the downloaded content
             temp_file = tempfile.NamedTemporaryFile(delete=False)
             temp_file_path = temp_file.name
-
             try:
                 response = requests.get(link)
                 if response.status_code == 200:
                     temp_file.write(response.content)
                 else:
                     return "Failed to fetch content from the provided link."
-
                 mime_type, _ = mimetypes.guess_type(link)
                 if mime_type == 'application/pdf':
                     content_text = extract_text_from_pdf(temp_file_path)
@@ -113,14 +163,12 @@ def index_from_link():
                     content_text = docx2txt.process(BytesIO(docx_content))
                 else:
                     return "Unsupported link format."
-
                 index = index_pdf(content_text)
                 results = perform_query(index, query)
                 return render_template('search_results.html', query=query, results=results)
             finally:
                 temp_file.close()
-                os.unlink(temp_file_path)  # Remove the temporary file
-
+                os.unlink(temp_file_path)
         return "Please provide both a link and a query."
     except Exception as e:
         return render_template('error.html', error_message=str(e))
@@ -130,49 +178,37 @@ def video_transcribe():
     try:
         video_link = request.form['video_link']
         query = request.form['query']
-
-        # Create a temporary directory
         temp_dir = tempfile.TemporaryDirectory()
-
-        # Define the temporary audio file paths
         temp_audio_mp3_path = os.path.join(temp_dir.name, 'youtube_audio_copy.mp3')
-        
         if video_link and query:
             try:
-                # Download the audio from the YouTube link
                 yt = YouTube(video_link)
                 audio_stream = yt.streams.filter(only_audio=True).first()
                 audio_stream.download(output_path=temp_dir.name, filename='youtube_audio_copy.mp3')
-                print("Audio downloaded successfully.")
-
-                # Load the whisper model and transcribe the audio
-                model = whisper.load_model("base")
                 result = model.transcribe(temp_audio_mp3_path)
                 transcribed_text = result["text"]
-                print(transcribed_text)
-
-                # ... Perform indexing based on 'transcribed_text' ...
                 index = index_pdf(transcribed_text)
-                print("Indexing successful")
-
-                # ... Perform query based on 'index' and 'query' ...
                 results = perform_query(index, query)
-                print("Query successful")
-
                 return render_template('search_results.html', query=query, results=results)
             except Exception as download_error:
                 return f"Error downloading the YouTube video: {str(download_error)}"
             finally:
-                # Clean up the temporary audio files
                 if os.path.exists(temp_audio_mp3_path):
                     os.remove(temp_audio_mp3_path)
-
     except Exception as e:
         return render_template('error.html', error_message=str(e))
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return app.send_static_file(filename)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    return "File too large. Max allowed size is 16MB.", 413
 
 @app.route('/')
 def search_form():
