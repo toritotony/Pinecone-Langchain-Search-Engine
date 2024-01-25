1c8ffefc6daac73ad5faeb971474a19f48e94345
toritotony <88003805+toritotony@users.noreply.github.com>
2024-01-24 22:17:24 -0800

    Dev - add auto create & delete index feature

--- utilities.py
+++ utilities.py
@@ -60,6 +60,8 @@ def index_pdf(content):
     pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
     index_name = "test"
 
+    if index_name not in pinecone.list_indexes():
+        pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
     for text_chunk in texts:
         try:
             docsearch = Pinecone.from_texts([text_chunk], embeddings, index_name=index_name)
@@ -83,10 +85,12 @@ def extract_text_from_pdf(pdf_path):
 
 def perform_query(index, query):
     try:
+        index_name = "test"
         docs = index.similarity_search(query)
         llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", request_timeout=1500, max_tokens=2048, temperature=0, openai_api_key=OPENAI_API_KEY)
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=docs, question=query)
+        pinecone.delete_index(delete_all=True, name=index_name)
     except Exception as e:
         app.logger.error(f"Error while performing query: {e}")
         return (f"Error: {e}")
