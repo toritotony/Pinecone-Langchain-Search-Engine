5b310c84dd0663a885c2ffbefd667d49bd10ac3d
toritotony <wolfeanthony584@gmail.com>
2024-04-04 22:59:13 -0700

    Venv reset after testing app local

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wolfe\OneDrive\Desktop\pinecone-test\venv
+set VIRTUAL_ENV=C:\Users\wolfe\OneDrive\Documents\GitHub\Pinecone-Langchain-Search-Engine-private\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- venv/Scripts/pdf2txt.py
+++ venv/Scripts/pdf2txt.py
@@ -1,4 +1,4 @@
-#!C:\Users\wolfe\OneDrive\Desktop\pinecone-test\venv\Scripts\python.exe
+#!C:\Users\wolfe\OneDrive\Documents\GitHub\Pinecone-Langchain-Search-Engine-private\venv\Scripts\python.exe
 """A command line tool for extracting text and images from PDF and
 output it to plain text, html, xml or tags."""
 import argparse
