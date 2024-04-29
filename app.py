e8c0efc1d1e4f72177325ec0b3cd57e2279cefee
toritotony <wolfeanthony584@gmail.com>
2024-04-28 18:34:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', config.PINECONE_API_KEY)
 PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', config.PINECONE_API_ENV)
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx'}
 
-#test
+#testt
 from routes import *
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
